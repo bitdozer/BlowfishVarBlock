d30acfc7ee75a9c2564ff008c670ede0722a19bd
Richard Diamond <rmdaustin@gmail.com>
2016-08-20 19:08:40 -0700

    Initial Commit
    Working demonstration of variable block size Blowfish implementation.  Starting on unit testing.

---
+++ BlowfishVarBlock.Tests/Native40bitBlockTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlowfishVarBlock;
+
+namespace BlowfishVarBlock.Tests
+{
+    [TestClass]
+    public class Native40bitBlockTests
+    {
+        byte[] key = new byte[] { 0x20, 0x36, 0xac, 0x45, 0xd0 };
+        [TestMethod]
+        public void UInt64_40Bit_Min()
+        {
+            const ulong sourceValue = ulong.MinValue;
+            ulong encryptedValue = ulong.MinValue;
+            ulong targetValue = ulong.MaxValue;
+
+            byte[] payload = new byte[5];
+            byte[] buffer = new byte[8];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(sourceValue), 0, payload, 0, 5);
+
+            var encryptor = new Blowfish(key);
+
+            encryptor.Encipher40(payload, payload.Length);
+
+            payload.CopyTo(buffer, 0);
+            encryptedValue = BitConverter.ToUInt64(buffer, 0);
+
+            // one failure mode would be if the encryption didn't happen at all
+            Assert.AreNotEqual(sourceValue, encryptedValue);
+
+            // this is the expected value
+            Assert.AreEqual((ulong)1042903492005, encryptedValue);
+
+            // use a separate decryptor instance to ensure no dependency on 
+            // encryptor stored state
+            var decryptor = new Blowfish(key);
+            decryptor.Decipher40(payload, payload.Length);
+
+            payload.CopyTo(buffer, 0);
+            targetValue = BitConverter.ToUInt64(buffer, 0);
+
+            // the output should convert back tot he original uint
+            Assert.AreEqual(sourceValue, targetValue);
+        }
+        [TestMethod]
+        public void UInt64_40Bit_Max()
+        {
+            const ulong sourceValue = (((ulong)1) << 40) - 1; // 40-bit int MaxValue
+            ulong encryptedValue = ulong.MinValue;
+            ulong targetValue = ulong.MaxValue;
+            
+
+            byte[] payload = new byte[5];
+            byte[] buffer = new byte[8];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(sourceValue), 0, payload, 0, 5);
+
+            var encryptor = new Blowfish(key);
+
+            encryptor.Encipher40(payload, payload.Length);
+
+            payload.CopyTo(buffer, 0);
+            encryptedValue = BitConverter.ToUInt64(buffer, 0);
+
+            // one failure mode would be if the encryption didn't happen at all
+            Assert.AreNotEqual(sourceValue, encryptedValue);
+
+            // this is the expected value
+            Assert.AreEqual((ulong)1036168798002, encryptedValue);
+
+            // use a separate decryptor instance to ensure no dependency on 
+            // encryptor stored state
+            var decryptor = new Blowfish(key);
+            decryptor.Decipher40(payload, payload.Length);
+
+            payload.CopyTo(buffer, 0);
+            targetValue = BitConverter.ToUInt64(buffer, 0);
+
+            // the output should convert back tot he original uint
+            Assert.AreEqual(sourceValue, targetValue);
+        }
+        [TestMethod]
+        public void Multiple_Blocks()
+        {
+            byte[] original = new byte[] { 0x20, 0x36, 0x45, 0xac, 0xd0, 0x20, 0x36, 0xac, 0x45, 0xd0, 0x20, 0x36, 0xac, 0x45, 0xd0 };
+            byte[] encypheredExpected = new byte[] { 52, 202, 24, 208, 6, 73, 45, 38, 128, 114, 73, 45, 38, 128, 114 };
+
+            byte[] payload = new byte[original.Length];
+
+            Buffer.BlockCopy(original, 0, payload, 0, original.Length);
+
+            var encryptor = new Blowfish(key);
+
+            encryptor.Encipher40(payload, payload.Length);
+
+            // one failure mode would be if the encryption didn't happen at all
+            CollectionAssert.AreNotEqual(original, payload);
+
+            // this is the expected value
+            CollectionAssert.AreEqual(encypheredExpected, payload);
+
+            // use a separate decryptor instance to ensure no dependency on 
+            // encryptor stored state
+            var decryptor = new Blowfish(key);
+            decryptor.Decipher40(payload, payload.Length);
+
+            // the output should convert back to the original array
+            CollectionAssert.AreEqual(original, payload);
+        }
+    }
+}
---
+++ BlowfishVarBlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlowfishVarBlock")]
+[assembly: AssemblyDescription("Blowfish implementation with variable block size option")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("richarddiamond.com")]
+[assembly: AssemblyProduct("Blowfish40")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3dec9eb0-7538-4da7-9603-7ad6748e2688")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
